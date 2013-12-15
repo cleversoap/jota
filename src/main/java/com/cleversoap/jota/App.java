b59b908a578ae78bcb939c01184edd9046aae269
cleversoap <clever@cleversoap.com>
2013-12-15 20:10:06 +1000

    Initial commit with lwjgl dependency in maven

---
+++ src/main/java/com/cleversoap/jota/App.java
@@ -0,0 +1,13 @@
+package com.cleversoap.jota;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
